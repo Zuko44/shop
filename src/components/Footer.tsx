45de072ccedc1697cc58996ef79305c246fd016a
Vladislav Kuryanov <vkuryanov@marfa-tech.com>
2025-02-07 15:36:37 +0300

    header init

---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+import styles from '../App.module.scss';
+
+export const Footer = () => {
+  return <footer className={styles.footer}>this is footer</footer>;
+};
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+import styles from '../App.module.scss';
+
+export const Header = () => {
+  return <header className={styles.header}>this is header</header>;
+};
