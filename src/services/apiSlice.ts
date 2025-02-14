86e76a6cde71b50d84be9fc1a244d07aa63c1ad7
Vladislav Kuryanov <vkuryanov@marfa-tech.com>
2025-02-14 17:44:29 +0300

    added env

--- src/services/apiSlice.ts
+++ src/services/apiSlice.ts
@@ -1,9 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Product } from '../types';
 
+const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || 'https://fallback-url.com';
+
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://fakestoreapi.com/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   tagTypes: ['Products'],
   endpoints: (builder) => ({
     getProducts: builder.query<Product[], void>({
